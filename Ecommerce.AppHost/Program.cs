252cb37d828aedc97c2445e92c915b316ba21154
Santosh Pujari <santosh.pujari@hotmail.com>
2024-10-14 17:45:56 -0500

    Added 2 MicroService, Lib for models and Aspire App host project

--- Ecommerce.AppHost/Program.cs
+++ Ecommerce.AppHost/Program.cs
@@ -1,4 +1,10 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
+var productApi = builder.AddProject<Ecommerce_ProductService>("apiservice-product");
+var orderApi = builder.AddProject<Ecommerce_OrderService>("apiservice-order");
+
+builder.AddProject<Ecommerce_Web>("webfrontend").WithReference(productApi).WithReference(orderApi);
 
 builder.Build().Run();
